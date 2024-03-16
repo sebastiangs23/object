fc05cbc0b6912b89af9314aaa10317d30a49d0f2
sebastiangs23 <sebastiangs2309@gmail.com>
2024-03-16 17:11:19 -0500

    object

---
+++ uri.js
@@ -0,0 +1,38 @@
+let objeto = {
+    "name": "REX Token",
+    "symbol": "REX",
+    "description": "Un token inspirado en Rex de Toy Story.",
+    "seller_fee_basis_points": 500,
+    "image": "https://example.com/path/to/rex_image.png",
+    "attributes": [
+        {
+            "trait_type": "Background",
+            "value": "Toy Story Room"
+        },
+        {
+            "trait_type": "Mood",
+            "value": "Happy"
+        }
+    ],
+    "collection": {
+        "name": "Pixar Characters",
+        "family": "Pixar"
+    },
+    "properties": {
+        "files": [
+            {
+                "uri": "https://example.com/path/to/rex_image.png",
+                "type": "image/png"
+            }
+        ],
+        "category": "image",
+        "creators": [
+            {
+                "address": "tuDireccionSolanaAqui",
+                "share": 100
+            }
+        ]
+    }
+}
+
+return objeto
